86ad45cc39e977abeb09842684451e9d6cdfff85
amit <amit98ch@gmail.com>
2020-05-10 22:19:03 +0530

    Set Static files and Media files ( set STATIC_ROOT,STATIC_URL, MEDIA_ROOT, MEDIA_URL on settings.py and url_pattern on url.py, Load static files on home_page.html, add css, js files, run configuration using collectstatic )
    Signed-off-by: amit <amit98ch@gmail.com>

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from .views import home_page, contact_page, login_Page, registerPage
 
@@ -25,3 +27,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
 ]
+if settings.DEBUG:
+    # set static files
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # type python manage.py collectstatic
