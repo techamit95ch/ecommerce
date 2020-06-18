c5d32099ff9f04b7d5c16d18386ae2e11757e918
amit <amit98ch@gmail.com>
2020-06-18 18:27:44 +0530

    deleing cart_id session
    Signed-off-by: amit <amit98ch@gmail.com>

--- cart/models.py
+++ cart/models.py
@@ -5,12 +5,13 @@ User = settings.AUTH_USER_MODEL
 
 # Create your models here.
 class Cart(models.Model):
-
-    user        = models.ForeignKey ( User, null=True, blank=True, on_delete=models.CASCADE)
-    products    = models.ManyToManyField( Product, blank=True, null=True)
-    total       = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
-    updated     = models.DateTimeField(auto_now=True)    
-    timestamp   = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, blank=True, null=True)
+    total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.id)
+
+# 4 cart model 04:46
--- cart/views.py
+++ cart/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 
+from .models import Cart
+
+
 # Create your views here.
 def cart_home(request):
     # print(request.session.session_key) # on the request
-    request.session['cart_id']=13
+    # request.session['cart_id']=13
     # to save username in session varriable
-    request.session['username']= request.user.username
+    # request.session['username']= request.user.username
     # print(request.session.get('username'))
-    return render(request,'cart/home.html',{})
+    # Deleteing cart id session varriable
+    del request.session['cart_id']
+
+    cart_id = request.session.get("cart_id", None)
+    # if cart_id doesn't exist in session then it will responce None. not error.
+
+    if cart_id is None:  # and isinstance(cart_id,int):
+        # print('create new cart')
+        cart_obj = Cart.objects.create(user=None)
+        request.session['cart_id'] = cart_obj.id
+    else:
+        print(cart_id)
+        cart_obj = Cart.objects.get(id=cart_id)
+        # print("cart id exists")
+
+    return render(request, 'cart/home.html', {})
