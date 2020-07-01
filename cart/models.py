f188c05216efaf18ddca9c653fd3ad53cbfd6f19
amit <amit98ch@gmail.com>
2020-07-01 12:40:16 +0530

    change view using new_or_get def in cart model

--- cart/models.py
+++ cart/models.py
@@ -6,9 +6,32 @@ User = settings.AUTH_USER_MODEL
 
 
 # Create your models here.
-class CartManager(models.Model):
+class CartManager(models.Manager):
+    # def get_or_create():
+    #     return obj,True
+    def new_or_get(self,request):
+        cart_id = request.session.get("cart_id", None)
+        qs = self.get_queryset().filter(id=cart_id)
+        if qs.count() == 1:
+            new_obj = False
+            cart_obj = qs.first()
+            if request.user.is_authenticated and cart_obj.user is None :
+                cart_obj.user = request.user
+                cart_obj.save()
+        else:
+            cart_obj = Cart.objects.new(user= request.user)
+            new_obj =True
+            
+            request.session['cart_id'] = cart_obj.id
+        return cart_obj, new_obj
+
     def new(self, user=None):
-        return self.model.objects.create(user=user)
+        print(user)
+        user_obj = None
+        if user is not None:
+            if user.is_authenticated:
+                user_obj = user
+        return self.model.objects.create(user=user_obj)
 
 
 class Cart(models.Model):
@@ -17,9 +40,10 @@ class Cart(models.Model):
     total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
     objects = CartManager()
 
     def __str__(self):
         return str(self.id)
 
-# 4 cart model 04:46
+
--- cart/views.py
+++ cart/views.py
@@ -11,20 +11,26 @@ def cart_create(user=None):
 
 
 def cart_home(request):
-    del request.session['cart_id']
+    cart_obj = Cart.objects.new_or_get(request)
+    # # del request.session['cart_id',None]
 
-    cart_id = request.session.get("cart_id", None)
-    # if cart_id is None:  # and isinstance(cart_id,int):
-    #     cart_obj = Cart.objects.create(user=None)
-    #     request.session['cart_id'] = cart_obj.id
+    # cart_id = request.session.get("cart_id", None)
+    # # if cart_id is None:  # and isinstance(cart_id,int):
+    # #     cart_obj = Cart.objects.create(user=None)
+    # #     request.session['cart_id'] = cart_obj.id
+    # # else:
+    # #     # print(cart_id)
+    # qs = Cart.objects.filter(id=cart_id)
+    # if qs.count() == 1:
+    #     cart_obj = qs.first()
+    #     if request.user.is_authenticated and cart_obj.user is None :
+    #         cart_obj.user = request.user
+    #         cart_obj.save()
     # else:
-    #     # print(cart_id)
-    qs = Cart.objects.filter(id=cart_id)
-    if qs.count() == 1:
-        cart_obj = qs.first()
-    else:
 
-        # cart_obj = cart_create()
-        cart_obj = Cart.objects.new(user=None)
-    request.session['cart_id'] = cart_obj.id
+    #     # cart_obj = cart_create()
+    #     cart_obj = Cart.objects.new(
+    #         user= request.user
+    #     )
+    # request.session['cart_id'] = cart_obj.id
     return render(request, 'cart/home.html', {})
